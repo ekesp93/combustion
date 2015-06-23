0c8168431eb56f2b8295c2a7aaf1efa5375a5492
Evan Kreft <ekreft2@uic.edu>
2015-06-23 12:15:52 -0500

    Temperature parsing

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 # Writes a sample point cloud to use with the example pointcloud reader
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # CONFIGURATION
 scale = 10
 
@@ -13,37 +13,63 @@ density = 500
 # output file name
 filename = "data.xyzb"
 
+
 class Point(object):
-    def __init__(self, x, y, z):
+    def __init__(self, x, y, z, temp):
         self.x = x
         self.y = y
         self.z = z
+        self.temp = temp
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # CODE
 import struct
 from random import *
 from math import *
+import time
+
 file = open(filename, 'wb')
 fileread = open("particle00027.vtk", 'r')
 points = []
+temps = []
 
-for x in range(1, 70000):
+for x in range(1, 68610):
     temp = fileread.readline()
     if x > 9:
         if temp == "\n":
             break
         else:
             parsed = temp.split(" ")
-            points.append(Point(float(parsed[6]), float(parsed[12]), float(parsed[18])))
+            points.append(Point(float(parsed[6]), float(parsed[12]), float(parsed[18]), 0.0))
+
+# read tempratures
+for y in range(1, 480230):
+    temp1 = fileread.readline()
+    if y > 4:
+        if temp1 == "\n":
+            break
+        else:
+            parsed = temp1.split(" ")
+            # print y, parsed
+            temps.append(float(parsed[6]))
+
+# match a particle with a single temprature record
+for x in range(0, points.__len__()):
+    points[x].temperature = temps[x]
+
+# test if the particles are correctly recorded
+for k in range(11939, 11949):
+    print k,points[k].x,points[k].y,points[k].z,points[k].temp
+
 
 def generatePoint(x, y, z):
     # generate plane with RGBA data values all set to 1.
-    hfy  = 0.5 + ((sin(x*4)) * cos(z) * cos(x)) / 2
-    r = 0.5 + (sin(x/4) + cos(z/4)) / 2
+    hfy = 0.5 + ((sin(x * 4)) * cos(z) * cos(x)) / 2
+    r = 0.5 + (sin(x / 4) + cos(z / 4)) / 2
     g = (hfy + r) / 2
     b = 1 - r
     return struct.pack('ddddddd', x, y, z, r, g, b, 1.0)
+
 
 def outputBlock(x, z):
     for fx in range(0, len(points)):
@@ -61,6 +87,5 @@ for x in range(0, extx):
 
 file.close()
 
-
-totPointsK = extx*extz*density*density/1000
+totPointsK = extx * extz * density * density / 1000
 print("total points: {0}K".format(totPointsK))
