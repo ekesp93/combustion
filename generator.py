6c2ca9aab5695c67870a7742b3d50cb6a60ab5e6
Evan Kreft <ekreft2@uic.edu>
2015-06-23 14:52:05 -0500

    Parsed all known usable data

--- generator.py
+++ generator.py
@@ -15,11 +15,14 @@ filename = "data.xyzb"
 
 
 class Point(object):
-    def __init__(self, x, y, z, temp):
+    def __init__(self, x, y, z, temp, velx, vely, velz):
         self.x = x
         self.y = y
         self.z = z
         self.temp = temp
+        self.velx = velx
+        self.vely = vely
+        self.velz = velz
 
 # -------------------------------------------------------------------------------
 # CODE
@@ -31,36 +34,28 @@ import time
 file = open(filename, 'wb')
 fileread = open("particle00027.vtk", 'r')
 points = []
-temps = []
 
-for x in range(1, 68610):
+for x in range(1, 960450):
     temp = fileread.readline()
-    if x > 9:
-        if temp == "\n":
-            break
-        else:
-            parsed = temp.split(" ")
-            points.append(Point(float(parsed[6]), float(parsed[12]), float(parsed[18]), 0.0))
-
-# read tempratures
-for y in range(1, 480230):
-    temp1 = fileread.readline()
-    if y > 4:
-        if temp1 == "\n":
-            break
-        else:
-            parsed = temp1.split(" ")
-            # print y, parsed
-            temps.append(float(parsed[6]))
-
-# match a particle with a single temprature record
-for x in range(0, points.__len__()):
-    points[x].temperature = temps[x]
-
-# test if the particles are correctly recorded
-for k in range(11939, 11949):
-    print k,points[k].x,points[k].y,points[k].z,points[k].temp
-
+    if x > 9 and x < 68610:
+        parsed = temp.split(" ")
+        points.append(Point(float(parsed[6]), float(parsed[12]), float(parsed[18]), 0.0, 0.0, 0.0, 0.0))
+    elif x > 68613 and x < 137214:
+        parsed = temp.split(" ")
+        points[x - 68614].temp = float(parsed[6])
+    elif x > 891848 and x < 960449:
+        parsed = temp.split(" ")
+        print parsed
+        k = 0
+        for y in range(0, len(parsed)):
+            if parsed[y] != '':
+                if k == 0:
+                    points[x - 891849].velx = float(parsed[y])
+                elif k == 1:
+                    points[x - 891849].vely = float(parsed[y])
+                elif k == 2:
+                    points[x - 891849].velz = float(parsed[y])
+                k += 1
 
 def generatePoint(x, y, z):
     # generate plane with RGBA data values all set to 1.
