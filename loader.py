4068f4bcb6eeffc6833bb0ec5d11ebb004f68146
Evan Kreft <ekreft2@uic.edu>
2015-06-30 15:10:10 -0500

    second camera

--- loader.py
+++ loader.py
@@ -26,10 +26,29 @@ pointCloudModel.options = "10000 100:1000000:20 20:100:10 6:20:5 0:5:5"
 scene.loadModel(pointCloudModel)
 
 pointCloud = StaticObject.create(pointCloudModel.name)
+pointCloud.setPosition(-15, 2, -5)
+pointCloud.setScale(30, 100, 100)
 # attach shader uniforms
 mat = pointCloud.getMaterial()
 mat.setProgram(pointProgram.name)
 mat.attachUniform(pointScale)
 
-getDefaultCamera().setPosition(0, 10, 50)
+getDefaultCamera().setPosition(0, 10, -35)
 getDefaultCamera().lookAt(pointCloud.getBoundCenter(), Vector3(0,1,0))
+
+pressure = getOrCreateCamera('press')
+pressure.setHeadOffset(Vector3(0, 2, 0))
+
+coutput = PixelData.create(250,250,PixelFormat.FormatRgba)
+pressure.getOutput(0).setReadbackTarget(coutput)
+pressure.getOutput(0).setEnabled(True)
+uim = UiModule.createAndInitialize()
+container = Container.create(ContainerLayout.LayoutVertical, uim.getUi())
+container.setStyleValue('fill', 'black')
+container.setPosition(Vector2(25, 25))
+container.setAlpha(1)
+img = Image.create(container)
+img.setData(coutput)
+
+pressure.setPosition(0, 10, 35)
+pressure.lookAt(pointCloud.getBoundCenter(), Vector3(0,1,0))
