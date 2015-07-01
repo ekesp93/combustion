53189daab5f993f443278438a9d9a6298c654e1e
Evan Kreft <ekreft2@uic.edu>
2015-07-01 13:59:17 -0500

    Creates second file with velocity

--- generator.py
+++ generator.py
@@ -11,11 +11,12 @@ extz = 10
 density = 500
 
 # output file name
-filename = "data.xyzb"
+filename1 = "data.xyzb"
+filename2 = "data2.xyzb"
 
 
 class Point(object):
-    def __init__(self, x, y, z, temp, velx, vely, velz):
+    def __init__(self, x, y, z, temp, velx, vely, velz, vel):
         self.x = x
         self.y = y
         self.z = z
@@ -23,6 +24,7 @@ class Point(object):
         self.velx = velx
         self.vely = vely
         self.velz = velz
+        self.vel = vel
 
 # -------------------------------------------------------------------------------
 # CODE
@@ -31,16 +33,18 @@ from random import *
 from math import *
 import time
 
-file = open(filename, 'wb')
+file = open(filename1, 'wb')
+file2 = open(filename2, 'wb')
 fileread = open("particle00027.vtk", 'r')
 points = []
 maxTemp = 0
+maxVel = 0
 
 for x in range(1, 960450):
     temp = fileread.readline()
     if x > 9 and x < 68610:
         parsed = temp.split(" ")
-        points.append(Point(float(parsed[6]), float(parsed[12]), float(parsed[18]), 0.0, 0.0, 0.0, 0.0))
+        points.append(Point(float(parsed[6]), float(parsed[12]), float(parsed[18]), 0.0, 0.0, 0.0, 0.0, 0.0))
     elif x > 68613 and x < 137214:
         parsed = temp.split(" ")
         points[x - 68614].temp = float(parsed[6])
@@ -56,18 +60,28 @@ for x in range(1, 960450):
                 elif k == 2:
                     points[x - 891849].velz = float(parsed[y])
                 k += 1
+                points[x - 891849].vel = math.sqrt(math.pow(points[x - 891849].velx, 2) +
+                                                math.pow(points[x - 891849].vely, 2) +
+                                                math.pow(points[x - 891849].velz, 2))
 
 for x in range(0, len(points)):
     if points[x].temp > maxTemp:
         maxTemp = points[x].temp
+    if points[x].velx > maxVel:
+        maxVel = points[x].velx
 
-def generatePoint(x, y, z, temp):
+def generatePoint1(x, y, z, temp):
     # generate plane with RGBA data values all set to 1.
     r = temp/maxTemp
     g = 0.0
     b = (maxTemp - temp)/maxTemp
     return struct.pack('ddddddd', x, y, z, r, g, b, 1.0)
 
+def generatePoint2(x, y, z, vel):
+    r = vel/maxVel
+    g = (maxVel - vel)/maxVel
+    b = 0.0
+    return struct.pack('ddddddd', x, y, z, r, g, b, 1.0)
 
 def outputBlock(x, z):
     for fx in range(0, len(points)):
@@ -75,8 +89,11 @@ def outputBlock(x, z):
         py = points[fx].y
         pz = points[fx].z
         temp = points[fx].temp
-        dataBytes = generatePoint(px, py, pz, temp)
+        vel = points[fx].vel
+        dataBytes = generatePoint1(px, py, pz, temp)
         file.write(dataBytes)
+        dataBytes = generatePoint2(px, py, pz, vel)
+        file2.write(dataBytes)
 
 # generate and output line by line
 for x in range(0, extx):
@@ -85,6 +102,7 @@ for x in range(0, extx):
         outputBlock(x, z)
 
 file.close()
+file2.close()
 
 totPointsK = extx * extz * density * density / 1000
 print("total points: {0}K".format(totPointsK))
